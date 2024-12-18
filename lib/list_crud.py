04e73aef7d6f0885d0c1dacc8d8f595b126fda44
kishmwangi <kishmwangi31@gmail.com>
2024-12-18 13:57:11 +0300

    list crud lab test

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,39 @@
 def create_an_empty_list():
-    return None
+    return []
 
 def create_a_list():
-    return None
+    return ['banana', 'apple', 'orange', 'kiwi']
+
 
 def add_element_to_end_of_list(l, element):
-    return None
+    
+    if l is None:
+        l = []
+    l.append(element)
+    return l
+
+    
+
 
 def add_element_to_start_of_list(l, element):
-    return None
-
+    if l is None:
+        l = []
+    l.insert(0, element)
+    return l
 def remove_element_from_end_of_list(l):
-    return None
+    if l:
+        l.pop()
+    return l 
 
 def remove_element_from_start_of_list(l):
-    return None
+    if l:
+        del l [0]
+    return l
 
 def retrieve_first_element_from_list(l):
-    return None
+    return l [0] if l else none
 
 def retrieve_element_from_index(l, index):
-    return None
-
+    return l [index] if l and 0 <= index < len(l) else None
 def retrieve_last_element_from_list(l):
-    return None
+    return l[-1] if l else none 
